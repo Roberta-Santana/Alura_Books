b48323cd61afd45c29a3ffa12863ad7420cf982b
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-05 10:53:49 -0300

    Inserindo livro - forEach, innerHTML, template string ${}

--- app/main.js
+++ app/main.js
@@ -2,10 +2,36 @@ let livros=[];
 const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI();
 
+const elementoParaInserirLivro = document.getElementById('livros');
+
 async function getBuscarLivrosDaAPI(){
     const res = await fetch(endpointDaApi);
-    livros =  res.json();
+    livros = await res.json();
     console.table(livros);
+    exibirLivrosNaTela(livros);
 };
 
+function exibirLivrosNaTela(listaDeLivros){
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivro.innerHTML +=`<div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo"> ${livro.titulo} </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+        `
+    });
+}
 
+
+
+// - forEach()
+let linguagens = ['Js', 'Python', 'C#'];
+/* for(i=0;i<linguagens.length;i++){
+    console.log(linguagens[i])
+} */
+/* linguagens.forEach(elemento => {
+    console.log(elemento)}
+) */
