32b04839fd280daad2b2daaffbac2acb18c722ec
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-05 17:43:16 -0300

    criando arquivos js exclusivo para cada método, chamando na index

--- app/main.js
+++ app/main.js
@@ -2,30 +2,12 @@ let livros=[];
 const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI();
 
-const elementoParaInserirLivro = document.getElementById('livros');
-
 async function getBuscarLivrosDaAPI(){
     const res = await fetch(endpointDaApi);
     livros = await res.json();
     console.table(livros);
     exibirLivrosNaTela(livros);
-};
-
-function exibirLivrosNaTela(listaDeLivros){
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivro.innerHTML +=`<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo"> ${livro.titulo} </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-        `
-    });
 }
-
-
 
 // - forEach()
 let linguagens = ['Js', 'Python', 'C#'];
---
+++ app/metodoForEach.js
@@ -0,0 +1,15 @@
+const elementoParaInserirLivro = document.getElementById('livros');
+
+function exibirLivrosNaTela(listaDeLivros){
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivro.innerHTML +=`<div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo"> ${livro.titulo} </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+        `
+    });
+}
