240c5df998e3ffd6f14847e89e71d137d4a569ec
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-08 07:12:45 -0300

    filtarLivrosDeFront

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,14 +1,25 @@
 //MÉTODO FILTER
 const idades=[10,22,42,16,50]
 const podeDirigir = idades.filter(idade=>{
-    return idade>=18;
-})
+    return idade>=18;})
 console.log(`Das idades: ${idades}, Podem dirigir: ${podeDirigir}`)
 
-function categoria(livros){
+/* function categoria(livros){
     const frontEnd = livros.filter(livro=>{
         return {...livro, categoria:'Front-end'}
     })
-    return frontEnd;
-}
+    return frontEnd;} */
+
+//APLICANDO NOS LIVROS
+const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront');
+
+btnFiltrarLivrosFront.addEventListener('click', filtrarLivrosDeFront);
+
+    function filtrarLivrosDeFront(){
+        let livrosFiltrados = livros.filter(livro => 
+            livro.categoria == 'front-end'); 
+        console.log(livrosFiltrados);
+    }
+
+
 
