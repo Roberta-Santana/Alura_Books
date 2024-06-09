3f290b0204050fb858967aaaa6fef1c0b01a4de1
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-09 20:21:03 -0300

    Função do escopo global

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -16,23 +16,36 @@ btnFiltrarLivrosFront.addEventListener('click', filtrarLivros);
 //APLICANDO NOS LIVROS
 //1- pegar todos os botões
 const botoes = document.querySelectorAll('.btn');
+let livrosFiltrados;
+let valor;
 //2 - Pegar a lista de botões, definir que a cada elemento da lista 
 //irá executar um evento, atraves de um click, fazendo um filter de front, back, dados...
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
-
 
 function filtrarLivros(){
     //3 - identificar o botão clicado, pegando o ID
     const elementoBtn = document.getElementById(this.id);
     //4 - passar o valor do ID para var categoria
     const categoria = elementoBtn.value;
-    
-    let livrosFiltrados = livros.filter(livro => 
-        livro.categoria == categoria); 
+    livrosFiltrados = categoria =='disponivel'? filtarPorDisponobilidade(): filtarPorCategoria(categoria);
     exibirLivrosNaTela(livrosFiltrados);
-
-
+    if(categoria == 'disponivel'){
+        exibirValorTotalDosLivrosDisponíveis();
+    }
 }
 
+function filtarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtarPorDisponobilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponíveis(){
+        elementoComValorTotalDeLivrosDisponiveis.innerHTML += `<div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${livro.reduce(((acc,livro) => acc+livro.preco))}</span></p>
+    </div>`
+}
 
 
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,16 +1,29 @@
 const elementoParaInserirLivro = document.getElementById('livros');
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis');
+
 
 function exibirLivrosNaTela(listaDeLivros){
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML ='';
   elementoParaInserirLivro.innerHTML = '';
     listaDeLivros.forEach(livro => {
-        elementoParaInserirLivro.innerHTML +=`<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo"> ${livro.titulo} </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-        `
+      const disponibilidade = (livro.quantidade >0)? 'livro__imagens' : 'livro__imagens indisponivel';
+      /* verificarDisponibilidadeDoLivro(livro); */
+      elementoParaInserirLivro.innerHTML +=`<div class="livro">
+      <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo"> ${livro.titulo} </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+      <span class="tag">${livro.categoria}</span>
+      </div>
+      `
     });
 }
+
+/* function verificarDisponibilidadeDoLivro(livro){
+  if(livro.quantidade>0){
+    return 'livro__imagens'
+  } else{
+    return 'livro__imagens indisponivel'
+  }
+} //a var disponibilidade substitui a class livro__imagens */
